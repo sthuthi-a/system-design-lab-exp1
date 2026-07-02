ef5dcb7770a51a951dd4a393ddd2ede1c6e1901b
sthuthi-a <sthuthi2410236@ssn.edu.in>
2026-07-02 14:51:33 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("System Design Lab - Exercise 1");
+        System.out.println("Java project configured successfully in VS Code");
+    }
+}
